e1a4c36e7209d9914a441785704e0d3946b1d039
marko.struna <marko.struna@90f16aac-5f68-11de-92c7-cf56be1892c0>
2009-06-29 12:09:03 +0000

    Moved files to one folder.

---
+++ trunk/JkDefrag/Source/JKDefragLog.h
@@ -0,0 +1,21 @@
+#ifndef __JKDEFRAGLOG_H__
+#define __JKDEFRAGLOG_H__
+
+class JKDefragLog
+{
+public:
+	JKDefragLog();
+	void LogMessage(WCHAR *Format, ...);
+	void SetLogFilename(WCHAR *fileName);
+
+	WCHAR MyName[MAX_PATH];
+	WCHAR MyShortName[MAX_PATH];
+
+	WCHAR *GetLogFilename();
+
+protected:
+private:
+	WCHAR LogFile[MAX_PATH];
+};
+
+#endif
